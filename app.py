a04ed378ca2486a16bc552d903f6dc1b497b05cd
Naomighina <155899566+Naomighina@users.noreply.github.com>
2024-07-21 21:07:16 +0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -10,7 +9,9 @@ st.title('Analisis Data Transaksi')
 data = pd.read_csv('transactions.csv')
 
 # Tampilkan informasi dasar tentang dataset
+st.write("Informasi Dataset:")
 st.write(data.info())
+st.write("Statistik Deskriptif:")
 st.write(data.describe())
 
 # Analisis Univariat
